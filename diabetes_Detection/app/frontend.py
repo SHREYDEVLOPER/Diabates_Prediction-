3dba3c744d684c9dcbfa0dd59ef262a23922179b
Shreyan Sharma <121509007+SHREYDEVLOPER@users.noreply.github.com>
2025-01-26 19:16:44 +0530

    Update frontend.py

--- diabetes_Detection/app/frontend.py
+++ diabetes_Detection/app/frontend.py
@@ -2,7 +2,7 @@ import streamlit as st
 import requests
 import json
 
-API_URL = "http://localhost:8000/predict"
+API_URL = "http://fastapi-app:8000/predict"
 
 st.title("Diabetes Prediction App")
 st.write("Enter the details below to make a prediction.")
